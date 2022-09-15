22957a711d847e4562d4278c38eefe4f06879223
Luke <101342819+onLuke@users.noreply.github.com>
2022-09-15 16:48:44 +0900

    chore:컴포넌트 디렉터리 구조 일부 변경
    QueryClientProvider, RecoilRoot 모두 index 파일에서 사용
    컴포넌트 파일에서 다시 세부파일로 나누지 않도록 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { lazy, Suspense, useEffect, useState } from 'react';
 import { BrowserRouter, Router, Route, Routes } from 'react-router-dom';
-import MainPage from './pages/MainPage/MainPage';
+import MainPage from './pages/MainPage';
 import ModalProvider from './contexts/ModalContext';
 import KakaoAuthPage from './pages/KakaoAuthPage';
 import NaverAuthPage from './pages/NaverAuthPage';
